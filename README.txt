b21cb936aaa50c61734c9f4caed0608adaf07a99
God <god@prismsus.org>
2014-04-09 16:56:21 -0400

    filled README part

---
+++ README.txt
@@ -0,0 +1,1 @@
+This program is for rolling a dice.
--- dice.py
+++ dice.py
@@ -1,3 +1,6 @@
 import random
-rnum = random.randint(1, 6)
-print rnum
+def int_input(rnum):
+	i = int(raw_input("How many sides do the dice have? "))
+	rnum = random.randint(1, i)
+	return rnum
+print int_input(0)
