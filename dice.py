a0f76be0f11d68824e66519699b9592854399f4b
God <god@prismsus.org>
2014-04-11 10:53:53 -0400

    Multi-dices added

--- dice.py
+++ dice.py
@@ -1,6 +1,11 @@
 import random
+
 def int_input(rnum):
 	i = int(raw_input("How many sides do the dice have? "))
-	rnum = random.randint(1, i)
+	j = int(raw_input("How many dices do you want? "))
+	temp = 0
+	while temp != j:
+		rnum.append(random.randint(1, i))
+		temp = temp + 1
 	return rnum
-print int_input(0)
+print int_input([])
