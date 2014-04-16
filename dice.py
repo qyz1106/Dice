c8f5229103a70aeee1b90cf924e238ec1da9790a
God <god@prismsus.org>
2014-04-16 11:01:44 -0400

    Correct bugs.

--- dice.py
+++ dice.py
@@ -5,6 +5,8 @@ def int_input(rnum):
 	j = int(raw_input("How many dices do you want? "))
 	temp = 0
 	while temp != j:
+		if i <= 1：
+			return "Your number of sides is wrong."
 		rnum.append(random.randint(1, i))
 		temp = temp + 1
 	return rnum
