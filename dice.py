77534f8a6cf02aa58c1c1feffd7cf8ccc85aee0a
God <god@prismsus.org>
2014-04-17 19:22:04 -0400

    The real code of upgrade version.

--- dice.py
+++ dice.py
@@ -1,14 +1,14 @@
 import random
 
 def int_input(rnum):
-	i = int(raw_input("How many sides do the dice have? "))
-	j = int(raw_input("How many dices do you want? "))
-	temp = 0
-	while temp != j:
-		rnum.append(random.randint(1, i))
-		temp = temp + 1
+	num = int(raw_input("How many dices do you want? "))
+	for i in range(1, num):
+		d[i] = raw_input("How many sides for the", i, "dice? ")
+	for i in range(1, num):
+		for j in range(1, d[i]):
+			rnum[i][j] = random.randint(1, j)
 	return rnum
-print int_input([])
+print int_input([][])
 
 #Ask user how many dices
 #Ask them how many sides
